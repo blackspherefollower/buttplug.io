aba3c409c754c542d38a104b56114980bcab88d4
Kyle Machulis <kyle@nonpolynomial.com>
2016-09-07 23:26:02 -0700

    Start pelicanizing SOLID

---
+++ pelicanconf.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'qDot'
+SITENAME = 'buttplug.io'
+SITEURL = 'https://buttplug.io'
+
+PATH = 'content'
+
+TIMEZONE = 'US/Pacific'
+
+DEFAULT_LANG = 'en'
+
+THEME = "themes/buttplug"
+
+# Feed generation is usually not desired when developing
+FEED_ATOM = ('atom.xml')
+FEED_RSS = ('index.xml')
+CATEGORY_FEED_ATOM = None
+FEED_ALL_ATOM = None
+FEED_DOMAIN = SITEURL
+TRANSLATION_FEED = None
+FEED_MAX_ITEMS = 10
+
+# Relative to content dir
+ARTICLE_PATHS = ['posts']
+ARTICLE_URL = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+CATEGORY_SAVE_AS = False
+TAG_URL = 'blog/tag/{slug}/index.html'
+TAG_SAVE_AS = 'blog/tag/{slug}/index.html'
+AUTHOR_SAVE_AS = False
+INDEX_SAVE_AS = 'blog/index.html'
+DIRECT_TEMPLATES = ['index', 'tags', 'archives']
+
+DAY_ARCHIVE_SAVE_AS = 'blog/{date:%Y}/{date:%m}/{date:%d}/index.html'
+MONTH_ARCHIVE_SAVE_AS = 'blog/{date:%Y}/{date:%m}/index.html'
+YEAR_ARCHIVE_SAVE_AS = 'blog/{date:%Y}/index.html'
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+PLUGIN_PATHS = ['plugins']
+PLUGINS = ['summary', 'neighbors', ]
+
+SUMMARY_END_MARKER = "<!--more-->"
+SUMMARY_MAX_LENGTH = None
+
+STATIC_PATHS = ['extras/htaccess',
+                'extras/robots.txt',
+                'images']
+
+EXTRA_PATH_METADATA = {
+    'extras/htaccess': {'path': '.htaccess'},
+    'extras/robots.txt': {'path': 'robots.txt'}
+}
+
+TEMPLATE_PAGES = {'templates/index.html': "index.html",
+                  'templates/404.html': "404/index.html"}
+
+# MENUITEMS = (('bio', '/about'),
+#              ('blog', '/'),
+#              ('portfolio', '/portfolio'))
+# #             ('config', '/config'))
+
+SITELOGO = "theme/images/buttplug.logo.png"
+SITELOGOSVG = "theme/images/buttplug.logo.svg"
+SITELOGO_SIZE = "35px"
+HIDE_SIDEBAR = True
+DISPLAY_PAGES_ON_MENU = False
+DISPLAY_ARCHIVES_ON_MENU = False
+DISPLAY_CATEGORIES_ON_MENU = False
+DISPLAY_BREADCRUMBS = False
+DISPLAY_ARTICLE_INFO_ON_INDEX = True
+SHOW_ARTICLE_CATEGORY = False
+
+PIWIK_URL = "apps.nonpolynomial.com/piwik"
+PIWIK_SITE_ID = "5"
+
+MD_EXTENSIONS = ['codehilite', 'extra', 'video']
+TWITTER_CARDS = True
+USE_OPEN_GRAPH = True
+TWITTER_USERNAME = "buttplugio"
+OPEN_GRAPH_IMAGE = "images/header-opengraph.jpg"
